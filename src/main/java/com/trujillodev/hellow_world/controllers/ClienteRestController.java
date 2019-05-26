d4be334dbedb2d9d6c36a7819d4f720cf9bacbd1
Manuel Guarniz <cruzemg95@gmail.com>
2019-05-26 01:56:16 -0500

    crud de clientes

--- src/main/java/com/trujillodev/hellow_world/controllers/ClienteRestController.java
+++ src/main/java/com/trujillodev/hellow_world/controllers/ClienteRestController.java
@@ -3,9 +3,16 @@ package com.trujillodev.hellow_world.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.trujillodev.hellow_world.models.entity.Cliente;
@@ -25,5 +32,35 @@ public class ClienteRestController {
 	public List<Cliente> index() {
 		// TODO Auto-generated method stub
 		return clienteService.findAll();
+	}
+	
+	@GetMapping("/clientes/{id}")
+	public Cliente show(@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente create(@RequestBody Cliente cliente) {
+		// cliente.setCreateAt(new Date());
+		return clienteService.save(cliente);
+	}
+	
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente clienteActual = clienteService.findById(id);
+		
+		clienteActual.setApellido(cliente.getApellido());
+		clienteActual.setNombre(cliente.getNombre());
+		clienteActual.setEmail(cliente.getEmail());
+		
+		return clienteService.save(clienteActual);
+	}
+	
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delte(@PathVariable Long id) {
+		clienteService.delete(id);
 	}
 }
